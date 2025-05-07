6a37f96c016955bc01708677036d19a4f430d85d
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-05-07 15:46:00 -0300

    user session when logging into the system, generates authentication token.

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,8 +1,32 @@
+import { AppError } from "@/utils/AppError";
 import { Request, Response } from "express";
+import { sign } from "jsonwebtoken";
+import { authConfig } from "@/configs/auth";
 
 class SessionsController {
   async create(request: Request, response: Response) {
-    return response.json({ message: "Sessions ok" });
+    const { username, password } = request.body;
+
+    //simulando um banco de dados
+    const fakeUser = {
+      id: 1,
+      username: "Bruno",
+      password: "123456",
+    };
+
+    //se não bate o username ou password - passado no  body - insomnia
+    if (username !== fakeUser.username || password !== fakeUser.password) {
+      throw new AppError("Username or password incorrect!", 401);
+    }
+
+    //retornando o token
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = sign({}, secret, {
+      subject: String(fakeUser.id),
+      expiresIn,
+    });
+
+    return response.json({ token });
   }
 }
 
